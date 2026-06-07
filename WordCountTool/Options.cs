d7cc75f363120aebca3bac213ee110b3430695bd
Thanasis Balamatsis <thanasis.b@yahoo.com>
2026-06-07 12:26:12 +0300

    Add Options class

---
+++ WordCountTool/Options.cs
@@ -0,0 +1,9 @@
+﻿namespace WordCountTool;
+
+static class Options
+{
+    public const string BytesCount = "-c";
+    public const string LinesCount = "-l";
+    public const string WordsCount = "-w";
+    public const string CharactersCount = "-m";
+}
